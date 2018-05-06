f3f848caef5f97a7914589290944285c267666e8
pfisterfarm <steve@pfisterfarm.com>
2018-05-05 21:51:19 -0400

    retrying push

--- app/src/main/java/com/pfisterfarm/popularmovies/MainActivity.java
+++ app/src/main/java/com/pfisterfarm/popularmovies/MainActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String sApiKey = BuildConfig.API_KEY;
+
+    ArrayList<Movie> popularMovies;
+    ArrayList<Movie> topRatedMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/pfisterfarm/popularmovies/Movie.java
+++ app/src/main/java/com/pfisterfarm/popularmovies/Movie.java
@@ -1,4 +1,55 @@
-public class Movie {
+package com.pfisterfarm.popularmovies;
 
-    
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+
+    private String title;
+    private String releaseDate;
+    private String posterPath;
+    private int voteAverage;
+    private String plotSynopsis;
+
+    protected Movie(String title, String releaseDate, String posterPath, int voteAverage, String plotSynopsis) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+        this.voteAverage = voteAverage;
+        this.plotSynopsis = plotSynopsis;
+    }
+
+    protected Movie(Parcel in) {
+        title = in.readString();
+        releaseDate = in.readString();
+        posterPath = in.readString();
+        voteAverage = in.readInt();
+        plotSynopsis = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(releaseDate);
+        dest.writeString(posterPath);
+        dest.writeInt(voteAverage);
+        dest.writeString(plotSynopsis);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
